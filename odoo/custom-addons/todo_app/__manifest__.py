781b18452e56e6db8d4cc6c71404479553f290ba
Varo1234 <alvaro18gp@gmail.com>
2021-09-28 16:28:58 -0500

    Initial commit

---
+++ odoo/custom-addons/todo_app/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'To-Do Application',
+    'description': 'Manage your personal
+    To-Do
+    tasks.', 
+    'author': 'Daniel Reis',
+    'depends': ['base'],
+    'application': True,
+}
